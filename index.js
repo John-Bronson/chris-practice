cfd19b121496d00dca8b44243d1e4b5ed9d9ca08
John Bronson <jbronson@gmail.com>
2022-05-16 13:24:54 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function oddNumbers(inputArray) {
+  var accumulator = []
+
+  for (let i = 0; i < inputArray.length; i++) {
+    console.log('index is ', i, 'value at that index is', inputArray[i])
+  }
+
+  return accumulator
+}
+
+oddNumbers([7, 8])
+//oddNumbers([1, 2, 3, 4, 5, 6])
